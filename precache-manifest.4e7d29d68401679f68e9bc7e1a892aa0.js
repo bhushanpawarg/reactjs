5b24bbc95a9d64e20dd0892a7ed36008e48fd2fc
Bhushan Pawar <bhushan.pawar23@gmail.com>
2020-02-22 01:23:05 +0530

    Updates

---
+++ precache-manifest.4e7d29d68401679f68e9bc7e1a892aa0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261b24d858548aeecf871d8a2676fb41",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "df060d11c26c4664b18f",
+    "url": "/myapp/static/css/main.5e283e14.chunk.css"
+  },
+  {
+    "revision": "c88cebed7dec8a545eeb",
+    "url": "/myapp/static/js/2.250dc4af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myapp/static/js/2.250dc4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df060d11c26c4664b18f",
+    "url": "/myapp/static/js/main.351126a7.chunk.js"
+  },
+  {
+    "revision": "085eaedc6753e841dd23",
+    "url": "/myapp/static/js/runtime-main.2b4fa720.js"
+  },
+  {
+    "revision": "094d8523a225faee9a2c5e7d290d0f48",
+    "url": "/myapp/static/media/advent-pro-v10-latin-regular.094d8523.woff"
+  },
+  {
+    "revision": "30b71a1e2036a397b136035da3a45af9",
+    "url": "/myapp/static/media/advent-pro-v10-latin-regular.30b71a1e.woff2"
+  },
+  {
+    "revision": "c6b8fb1f96c8fd7046ea9be09793c097",
+    "url": "/myapp/static/media/advent-pro-v10-latin-regular.c6b8fb1f.svg"
+  },
+  {
+    "revision": "d0e861bdc37b6cd1823069a9f3ebb9d9",
+    "url": "/myapp/static/media/advent-pro-v10-latin-regular.d0e861bd.eot"
+  },
+  {
+    "revision": "f6f31ed5d8b803bb6a4a00364db3fbdb",
+    "url": "/myapp/static/media/advent-pro-v10-latin-regular.f6f31ed5.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "/myapp/static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/myapp/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "4be1a572fca40bcb2202504cb17aed91",
+    "url": "/myapp/static/media/roboto-v20-latin-regular.4be1a572.eot"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/myapp/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "/myapp/static/media/roboto-v20-latin-regular.8681f434.svg"
+  }
+]);
